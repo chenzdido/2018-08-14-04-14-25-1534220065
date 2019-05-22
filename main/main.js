e9791759ca0a8580da56d1482ff4f8ad93167ed2
chenz <chenzhedido@outlook.com>
2019-05-22 20:23:13 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,20 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+    var str=number.toString();
+	var s1=["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
+	var s2=["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+	var s3=["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+	var l=str.length;
+	var num=new Array();
+	var ss1="",ss2="",ss3="";
+	for(var i=0;i<l;i++)
+	{
+		ss1+=s1[str[i]];
+		ss2+=s2[str[i]];
+		ss3+=s3[str[i]];
+	}
+	num.push(ss1);
+	num.push(ss2);
+	num.push(ss3);
+	var sk=num.join('\n');
+	console.log(sk); 
 };
